a9e22b77ae5a577ed30be8e077ac723ccf9a303d
fy <tofengyun@gmail.com>
2023-03-05 23:24:05 +0800

    make traffic statistics more flexible.

--- ss-libev-manager.sh
+++ ss-libev-manager.sh
@@ -23,56 +23,71 @@ configs=(
 allow_list=()
 allow_list_ip6=()
 
+get_icmp_prohibited() { if [ $1 == iptables ]; then echo icmp-host-prohibited; else echo icmp6-adm-prohibited; fi }
+
+do_if_not_exist() {
+    monitor=$(eval $1)
+    if [ ${#monitor} == 0 ]; then
+        eval $2
+    fi
+}
+
+check_chains() {
+    do_if_not_exist   "$1 -nvL | grep SSIN"            "$1 -N SSIN"
+    do_if_not_exist   "$1 -nvL INPUT | grep SSIN"      "$1 -A INPUT -j SSIN"
+    do_if_not_exist   "$1 -nvL | grep SSOUT"           "$1 -N SSOUT"
+    do_if_not_exist   "$1 -nvL OUTPUT | grep SSOUT"    "$1 -A OUTPUT -j SSOUT"
+}
+
+accept_all() {
+    do_if_not_exist    "$1 -nvL | grep 'tcp dpt:$2'"    "$1 -A SSIN -p tcp --dport $2"
+    do_if_not_exist    "$1 -nvL | grep 'udp dpt:$2'"    "$1 -A SSIN -p udp --dport $2"
+    do_if_not_exist    "$1 -nvL | grep 'tcp spt:$2'"    "$1 -A SSOUT -p tcp --sport $2"
+    do_if_not_exist    "$1 -nvL | grep 'udp spt:$2'"    "$1 -A SSOUT -p udp --sport $2"
+}
+
+accept_ip() {
+    do_if_not_exist    "$1 -nvL | grep '$2.*tcp dpt:$3'"    "$1 -A SSIN -p tcp -s $2 --dport $3 -j ACCEPT"
+    do_if_not_exist    "$1 -nvL | grep '$2.*udp dpt:$3'"    "$1 -A SSIN -p udp -s $2 --dport $3 -j ACCEPT"
+    do_if_not_exist    "$1 -nvL | grep '$2.*tcp spt:$3'"    "$1 -A SSOUT -p tcp -d $2 --sport $3 -j ACCEPT"
+    do_if_not_exist    "$1 -nvL | grep '$2.*udp spt:$3'"    "$1 -A SSOUT -p udp -d $2 --sport $3 -j ACCEPT"
+}
+
+reject_all() {
+    icmp_prohibited=$(get_icmp_prohibited $1)
+    do_if_not_exist    "$1 -nvL | grep 'REJECT.*tcp dpt:$2'"    "$1 -A SSIN -p tcp --dport $2 -j REJECT --reject-with tcp-reset"
+    do_if_not_exist    "$1 -nvL | grep 'REJECT.*udp dpt:$2'"    "$1 -A SSIN -p udp --dport $2 -j REJECT --reject-with $icmp_prohibited"
+    do_if_not_exist    "$1 -nvL | grep 'REJECT.*tcp spt:$2'"    "$1 -A SSOUT -p tcp --sport $2 -j REJECT --reject-with tcp-reset"
+    do_if_not_exist    "$1 -nvL | grep 'REJECT.*udp spt:$2'"    "$1 -A SSOUT -p udp --sport $2 -j REJECT --reject-with $icmp_prohibited"
+}
+
+set_rules() {
+    cmd=$1
+    port=$2
+    if [ $cmd == iptables ]; then allow_list=($(echo ${allow_list_v4[@]})); else allow_list=($(echo ${allow_list_v6[@]})); fi
+    if [ ${#allow_list[@]} -ne 0 ]; then
+        for ip in ${allow_list[@]}; do
+            accept_ip $cmd $ip $port
+        done
+        reject_all $cmd $port
+    else
+        accept_all $cmd $port
+    fi
+    echo "Add $port $cmd monitor."
+    if [ ${#monitor} -ne 0 ]; then
+        echo "No changes with $1."
+    fi
+}
+
 do_start() {
+    check_chains iptables
+    check_chains ip6tables
     local_port=1080
     for line in "${configs[@]}"; do
         config=($line)
         nohup ${ssserver} -c ${config[0]} -l $local_port -p ${config[1]} -k ${config[2]} -m ${config[3]} > /var/log/ss-libev-manager/${config[1]}.log &
-        monitor=$(iptables -L -v -n | grep spt:${config[1]})
-        monitor6=$(ip6tables -L -v -n | grep spt:${config[1]})
-        if [ ${#monitor} == 0 ]; then
-            if [ ${#allow_list[@]} -ne 0 ]; then
-                for ip in ${allow_list[@]}; do
-                    iptables -A INPUT -p tcp -s $ip --dport ${config[1]} -j ACCEPT
-                    iptables -A INPUT -p udp -s $ip --dport ${config[1]} -j ACCEPT
-                    iptables -A OUTPUT -p tcp -d $ip --sport ${config[1]} -j ACCEPT
-                    iptables -A OUTPUT -p udp -d $ip --sport ${config[1]} -j ACCEPT
-                done
-                iptables -A INPUT -p tcp --dport ${config[1]} -j REJECT --reject-with tcp-reset
-                iptables -A INPUT -p udp --dport ${config[1]} -j REJECT --reject-with icmp-host-prohibited
-                iptables -A OUTPUT -p tcp --sport ${config[1]} -j REJECT --reject-with tcp-reset
-                iptables -A OUTPUT -p udp --sport ${config[1]} -j REJECT --reject-with icmp-host-prohibited
-            else
-                iptables -A INPUT -p tcp --dport ${config[1]}
-                iptables -A INPUT -p udp --dport ${config[1]}
-                iptables -A OUTPUT -p tcp --sport ${config[1]}
-                iptables -A OUTPUT -p udp --sport ${config[1]}
-            fi
-            echo "Add iptables monitor."
-        fi
-        if [ ${#monitor6} == 0 ]; then
-            if [ ${#allow_list_ip6[@]} -ne 0 ]; then
-                for ip in ${allow_list_ip6[@]}; do
-                    ip6tables -A INPUT -p tcp -s $ip --dport ${config[1]} -j ACCEPT
-                    ip6tables -A INPUT -p udp -s $ip --dport ${config[1]} -j ACCEPT
-                    ip6tables -A OUTPUT -p tcp -d $ip --sport ${config[1]} -j ACCEPT
-                    ip6tables -A OUTPUT -p udp -d $ip --sport ${config[1]} -j ACCEPT
-                done
-                ip6tables -A INPUT -p tcp --dport ${config[1]} -j REJECT --reject-with tcp-reset
-                ip6tables -A INPUT -p udp --dport ${config[1]} -j REJECT --reject-with icmp6-adm-prohibited
-                ip6tables -A OUTPUT -p tcp --sport ${config[1]} -j REJECT --reject-with tcp-reset
-                ip6tables -A OUTPUT -p udp --sport ${config[1]} -j REJECT --reject-with icmp6-adm-prohibited
-            else
-                ip6tables -A INPUT -p tcp --dport ${config[1]}
-                ip6tables -A INPUT -p udp --dport ${config[1]}
-                ip6tables -A OUTPUT -p tcp --sport ${config[1]}
-                ip6tables -A OUTPUT -p udp --sport ${config[1]}
-            fi
-            echo "Add ip6tables monitor."
-        fi
-        if [ ${#monitor} -ne 0 ] && [ ${#monitor6} -ne 0 ]; then
-            echo "No changes with iptables and ip6tables."
-        fi
+        set_rules iptables ${config[1]}
+        set_rules ip6tables ${config[1]}
         let "local_port++"
         echo "Port" ${config[1]} "with encrypt" ${config[3]} "started."
     done
